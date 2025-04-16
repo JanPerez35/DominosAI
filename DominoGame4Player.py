92ee233e2d787bf2b23eecd2fe2db69383279d86
Umana-Manuel <manuel.umana@upr.edu>
2025-04-16 12:03:09 -0400

    Changes made previosly

--- DominoGame4Player.py
+++ DominoGame4Player.py
@@ -185,12 +185,13 @@ class DominoGUI:
                 self.draw_hand()
             return
 
+
         cp = self.game.current_player
         hand = self.game.players[cp]
         valid_moves = self.game.get_valid_moves(hand)
 
         while not valid_moves and self.game.stock:
-            drawn = self.game.draw_from_stock(cp) # Se borra pq no se usa???
+            drawn = self.game.draw_from_stock(cp)  # Se borra pq no se usa???
             valid_moves = self.game.get_valid_moves(self.game.players[cp])
 
         move = self.monte_carlo_ai_move(cp, simulations=25)
@@ -204,9 +205,8 @@ class DominoGUI:
         self.draw_board()
         self.update_ai_tile_counts()
         self.game.current_player = (self.game.current_player + 1) % 4
-            
-        self.root.after(1000, self.ai_turn) # delay between AI turns
-        
+        self.root.after(1000, self.ai_turn)  # delay between AI turns
+
     def monte_carlo_ai_move(self, player_index, simulations=25):
         hand = self.game.players[player_index]
         valid_moves = self.game.get_valid_moves(hand)
